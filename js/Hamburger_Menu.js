a0dec81e114d32c9ec60c38638327b22778af922
Q33Ls <Adalvie07@gmail.com>
2023-04-26 16:39:22 +0200

    Most of the layout html and css are done , just background colors and mobile responsiveness need to be corrected

---
+++ js/Hamburger_Menu.js
@@ -0,0 +1,5 @@
+
+    // Toggle menu dropdown on click
+    document.querySelector(".hamburger").addEventListener("click", function() {
+    document.querySelector(".menu-dropdown").classList.toggle("show");
+});
